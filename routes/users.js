2a0246982a0e91f215563f568a5bd19fb7ae6771
ESHamza <essaadihamza4@outlook.com>
2022-10-23 21:11:28 +0100

    store heshed password in the dtabase

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,21 @@
 const { User, validate } = require("../models/user"),
   express = require("express"),
   _ = require("lodash"),
+  bcrypt = require("bcrypt"),
   router = express.Router();
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
-  if (error) return res.status(404).send(error.details[0].message);
+  if (error) return res.status(400).send(error.details[0].message);
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(404).send("this user is already resitred!");
+  if (user) return res.status(400).send("this user is already resitred!");
 
   try {
     user = new User(_.pick(req.body, ["name", "email", "password"]));
+    const slat = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, slat);
     await user.save();
-    res.send(_.pick(user, ["name", "email"]));
+    res.send(_.pick(user, ["name", "email", "password"]));
   } catch (e) {
     console.log(e.message);
     return res.status(404).send(e.message);
