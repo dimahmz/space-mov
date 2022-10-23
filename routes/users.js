e23280691fd6d602d0f5861109ddca3c867f644f
ESHamza <essaadihamza4@outlook.com>
2022-10-23 18:27:33 +0100

    used lodash library

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,21 @@
 const { User, validate } = require("../models/user"),
   express = require("express"),
+  _ = require("lodash"),
   router = express.Router();
+
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(404).send(error.details[0].message);
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(404).send("this user is already resitred!");
+
   try {
-    user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
+    user = new User(_.pick(req.body, ["name", "email", "password"]));
     await user.save();
-    res.send(user);
+    res.send(_.pick(user, ["name", "email"]));
   } catch (e) {
-    console.log(e);
-    return res.status(404).send(e);
+    console.log(e.message);
+    return res.status(404).send(e.message);
   }
 });
 
